2eb7aaa37b3ede7f1390d9b02fc39d51c58da4be
manaratef570 <58149620+manaratef570@users.noreply.github.com>
2020-12-27 12:37:59 +0200

    Add files via upload

---
+++ src/main/java/com/javatpoint/repository/EmailRepository.java
@@ -0,0 +1,8 @@
+package com.javatpoint.repository;
+import com.javatpoint.model.Notification;
+import org.springframework.data.repository.CrudRepository;
+public interface EmailRepository extends CrudRepository<Notification, Integer>  {
+
+
+        }
+
