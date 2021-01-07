63adb1f0f64efdb35219f024dcd0bd1c12048a1d
Khaledmarcello <66645843+Khaledmarcello@users.noreply.github.com>
2021-01-07 18:58:20 +0200

    Refractoring Files

--- src/main/java/com/javatpoint/repository/EmailRepository.java
+++ src/main/java/com/javatpoint/repository/EmailRepository.java
@@ -2,7 +2,6 @@ package com.javatpoint.repository;
 
 import com.javatpoint.model.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/javatpoint/repository/NotificationsRepository.java
+++ src/main/java/com/javatpoint/repository/NotificationsRepository.java
@@ -2,7 +2,6 @@ package com.javatpoint.repository;
 
 import com.javatpoint.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationsRepository extends JpaRepository<Notification, Integer> {
 }
